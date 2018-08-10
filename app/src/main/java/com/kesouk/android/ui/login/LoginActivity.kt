bee28210c02ff8d8ccb23b7ed2dd5d6846723353
lakshmi <lakshmiprasanna504@gmail.com>
2018-08-10 11:20:03 -0400

    drawable files

--- app/src/main/java/com/kesouk/android/ui/login/LoginActivity.kt
+++ app/src/main/java/com/kesouk/android/ui/login/LoginActivity.kt
@@ -6,7 +6,9 @@ import android.os.Bundle
 import android.view.View
 import com.kesouk.android.R
 import com.kesouk.android.ui.base.BaseActivity
+import com.kesouk.android.ui.register.RegisterActivity
 import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_login_register.*
 
 class LoginActivity : AppCompatActivity(),View.OnClickListener {
 
@@ -14,10 +16,23 @@ class LoginActivity : AppCompatActivity(),View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        btn_log.setOnClickListener(this)
+        btn_reg.setOnClickListener(this)
 
 }
-    override fun onClick(p0: View?) {
+    override fun onClick(view: View?) {
 
+        when (view!!.id) {
+            R.id.btn_log -> {
+                var intentLogin = Intent(applicationContext, BaseActivity::class.java)
+                startActivity(intentLogin)
+            }
+            R.id.btn_reg -> {
+                var intentReg = Intent(applicationContext, RegisterActivity::class.java)
+                startActivity(intentReg)
+            }
+
+        }
     }
 
 }
